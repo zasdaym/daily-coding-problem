0912e4aab410711c58fff6345bbb43f033cb2f24
Zasda Yusuf Mikail <zasdaym@gmail.com>
2021-03-03 09:58:29 +0700

    Add Problem #2

---
+++ problem-002/solution.py
@@ -0,0 +1,9 @@
+import math
+
+def arr_product(numbers):
+    product = math.prod(numbers)
+    result = [product/number for number in numbers]
+    return result
+
+assert arr_product([1, 2, 3, 4, 5]) == [120, 60, 40, 30, 24]
+assert arr_product([3, 2, 1]) == [2, 3, 6]
