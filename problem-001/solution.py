7a35309b4504b465378a885ac706d2bcd2c1e628
Zasda Yusuf Mikail <zasdaym@gmail.com>
2021-03-05 11:03:01 +0700

    Update Problem #1 solution so use set and typing

--- problem-001/solution.py
+++ problem-001/solution.py
@@ -1,10 +1,12 @@
-def two_sum(numbers, target):
-    visited = {}
+from typing import Dict, List, Set
+
+def two_sum(numbers: List[int], target: int) -> bool:
+    visited: Set[int] = set()
     for number in numbers:
         diff = target - number
         if diff in visited:
             return True
-        visited[number] = True
+        visited.add(number)
     return False
 
 assert two_sum([10, 15, 3, 7], 17)
