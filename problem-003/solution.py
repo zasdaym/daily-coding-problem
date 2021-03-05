4337881d0bfecb582531d8fccd5d57510295d679
Zasda Yusuf Mikail <zasdaym@gmail.com>
2021-03-05 11:04:17 +0700

    Update Problem #3 to use typing

--- problem-003/solution.py
+++ problem-003/solution.py
@@ -1,12 +1,14 @@
+from typing import List
+
 class Node:
-    def __init__(self, val, left=None, right=None):
+    def __init__(self, val: str, left: Node = None, right: Node = None):
         self.val = val
         self.left = left
         self.right = right
 
 END_MARKER = "end"
 
-def serialize(root):
+def serialize(root: Node) -> str:
     if root is None:
         return END_MARKER
 
@@ -14,11 +16,11 @@ def serialize(root):
     right = serialize(root.right)
     return f"{root.val},{left},{right}"
 
-def deserialize(tree_string):
+def deserialize(tree_string: str) -> Node:
     values = tree_string.split(",")[::-1]
     return _deserialize(values)
 
-def _deserialize(values):
+def _deserialize(values: List[str]) -> Node:
     if not values:
         return None
 
