3861149b15ebd1f8dbc89c235920362bdb28a718
Zasda Yusuf Mikail <zasdaym@gmail.com>
2021-03-19 14:39:47 +0700

    Add Problem #18

---
+++ problem-018/solution.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+def max_value_each_subarray(numbers, size):
+    # deque to store indices of number for each subarray in descending order by value
+    index_deque = deque()
+
+    for i in range(len(numbers)):
+        # remove old indices in deque that is outside the current window (subarray)
+        while index_deque and index_deque[0] <= i-size:
+            index_deque.popleft()
+
+        # make sure deque is stored descendingly
+        while index_deque and numbers[i] >= numbers[index_deque[-1]]:
+            index_deque.pop()
+        index_deque.append(i)
+
+        if i >= size - 1:
+            print(numbers[index_deque[0]], end=" ")
+
+if __name__=="__main__":
+    numbers = [10, 5, 2, 7, 8, 7]
+    size = 3
+    max_value_each_subarray(numbers, 3)
