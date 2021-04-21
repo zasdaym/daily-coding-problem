eaaa8889a77441c10ee069a6e9c92579883fcecb
Zasda Yusuf Mikail <zasdaym@gmail.com>
2021-04-21 11:26:06 +0700

    Update Problem #51

--- problem-051/solution.py
+++ problem-051/solution.py
@@ -3,10 +3,17 @@ from random import randint
 
 
 def shuffle_deck(deck: List[int]) -> None:
+    """
+    This is called Fisher-Yates algorithm.
+    1. Iterate from the back of the array, index as i.
+        1. Generate a random integer j where 0 <= j <= i.
+        2. Swap arr[i] and arr[j]
+    """
     last_index = len(deck) - 1
     for i in range(last_index, 0, -1):
         j = randint(0, i+1)
         deck[i], deck[j] = deck[j], deck[i]
+
 
 original_deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 test_deck = original_deck.copy()
