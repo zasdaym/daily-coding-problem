26ed6638556a0ebc70626eff784e8d8c75b2c7d3
Zasda Yusuf Mikail <zasdaym@gmail.com>
2021-05-06 21:31:16 +0700

    Update Problem #66

--- problem-066/solution.py
+++ problem-066/solution.py
@@ -7,6 +7,11 @@ def biased_toss() -> int:
     return 1
 
 def unbiased_toss() -> int:
+    """
+    1. Toss the coin twice.
+    2. If the results match, start over, forgetting both results.
+    3. If the results differ, use the first result.
+    """
     diff = biased_toss() - biased_toss()
     if diff < 0:
         return 0
