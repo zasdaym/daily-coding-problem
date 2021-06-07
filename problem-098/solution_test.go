4b04c279e4ba74abeec855ee7beeb00628e6560a
Zasda Yusuf Mikail <zasdaym@gmail.com>
2021-06-07 20:46:54 +0700

    Add Problem #98 Go test case

--- problem-098/solution_test.go
+++ problem-098/solution_test.go
@@ -1,6 +1,8 @@
 package problem098
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestFindWord(t *testing.T) {
 	board := [][]byte{
@@ -11,16 +13,25 @@ func TestFindWord(t *testing.T) {
 
 	testCases := []struct {
 		name     string
+		board    [][]byte
 		target   string
 		expected bool
 	}{
 		{
+			name:     "empty board",
+			board:    [][]byte{},
+			target:   "randomstring",
+			expected: false,
+		},
+		{
 			name:     "existent word",
+			board:    board,
 			target:   "ABCCED",
 			expected: true,
 		},
 		{
 			name:     "non-existent word",
+			board:    board,
 			target:   "ABCA",
 			expected: false,
 		},
@@ -28,7 +39,7 @@ func TestFindWord(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			got := findWord(board, tc.target)
+			got := findWord(tc.board, tc.target)
 			if tc.expected != got {
 				t.Errorf("want %v got %v", tc.expected, got)
 			}
