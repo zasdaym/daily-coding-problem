85dcb3b1b966200c639ec057e981b3f4237bfe53
Zasda Yusuf Mikail <zasdaym@gmail.com>
2021-06-10 17:43:37 +0700

    Format Problem #101

--- problem-101/solution.py
+++ problem-101/solution.py
@@ -7,15 +7,17 @@ def prime_sum(num: int) -> Tuple[int, int]:
             return (i, num - i)
     return None
 
+
 def is_prime(num: int) -> bool:
     if num < 2:
         return False
-    
+
     for i in range(2, int(num**0.5)):
         if num % i == 0:
             return False
-    
+
     return True
+
 
 assert not is_prime(12)
 assert is_prime(13)
