0074c5115088f3493497f45228cfb5e058501feb
Zasda Yusuf Mikail <zasdaym@gmail.com>
2021-06-23 13:22:23 +0700

    Update Problem #112

--- problem-112/solution.py
+++ problem-112/solution.py
@@ -1,18 +1,13 @@
 class TreeNode:
-    def __init__(self, val):
-        self.left = None
-        self.right = None
-        self.parent = None
+    def __init__(self, val: int) -> None:
+        self.left: 'TreeNode' = None
+        self.right: 'TreeNode' = None
+        self.parent: 'TreeNode' = None
         self.val = val
 
-def lca(root, a, b):
-    def depth(node):
-        count = 0
-        while node:
-            count += 1
-            node = node.parent
-        return count
 
+def lca(root: TreeNode, a: TreeNode, b: TreeNode) -> TreeNode:
+    # Make both nodes on the same depth
     depth_a, depth_b = depth(a), depth(b)
     if depth_a < depth_b:
         while depth_a < depth_b:
@@ -23,8 +18,20 @@ def lca(root, a, b):
             a = a.parent
             depth_a -= 1
 
+    # Climb both nodes
     while a and b and (a is not b):
         a = a.parent
         b = b.parent
 
     return a if (a is b) else None
+
+
+def depth(node: TreeNode) -> int:
+    """
+    Get given node depth.
+    """
+    count = 0
+    while node:
+        count += 1
+        node = node.parent
+    return count
