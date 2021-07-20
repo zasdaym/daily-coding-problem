6404415321660d9dfd70e1381a32d8f8931f3cd1
Zasda Yusuf Mikail <zasdaym@gmail.com>
2021-07-20 11:19:08 +0700

    Add Problem #138

---
+++ problem-138/solution.py
@@ -0,0 +1,29 @@
+from typing import List
+from unittest import TestCase
+
+
+def min_coins(target: int) -> int:
+    coins = [25, 10, 5, 1]
+    total_count = 0
+
+    # Try to substract the target with each available coins, start with biggest first.
+    for coin in coins:
+        count = target // coin
+        target -= count * coin
+        total_count += count
+
+    return total_count
+
+
+class Test(TestCase):
+    def test_min_coins(self):
+        tests = [
+            (16, 3),
+            (70, 4),
+            (30, 2),
+        ]
+
+        for test in tests:
+            target, expected = test
+            got = min_coins(target)
+            self.assertEqual(expected, got)
